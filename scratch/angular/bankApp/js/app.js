447e40db83bcc7c166d4e59cc5172ca9f5d682be
runnerdave <runnerdave@gmail.com>
2016-07-14 19:18:36 +1000

    css and more functionality

--- scratch/angular/bankApp/js/app.js
+++ scratch/angular/bankApp/js/app.js
@@ -11,6 +11,19 @@
 			                  {"name":"Jimmy Verde", "balance":1923, "address":"small hometown"},
 			                  {"name":"Hugo Chang", "balance":1289, "address":"medium hometown"}
   			];
+  			$scope.addNew = function(user) {
+  				$scope.users.push(user);
+  			};
+  			$scope.remove = function(user) {
+  				var index = $scope.users.indexOf(user);
+  				alert("Deleting user: " + index);
+  				$scope.users.splice(index, 1);
+  			};
+  			$scope.edit = function(user) {
+  				$scope.current = user;  				
+  			};
+  			$scope.current = {};
 		}
+
 
 	);
