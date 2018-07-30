6c2cb913a1694d492f71dc7d565972b50faeafde
runnerdave <runnerdave@gmail.com>
2018-07-31 06:49:23 +1000

    filesystem prac

---
+++ scratch/fs/dirStat.js
@@ -0,0 +1,21 @@
+const _getAllFilesFromFolder = (dir) => {
+
+    const filesystem = require("fs");
+    let results = [];
+
+    filesystem.readdirSync(dir).forEach( (file) => {
+
+        file = dir + '/' + file;
+        const stat = filesystem.statSync(file);
+
+        if (stat && stat.isDirectory()) {
+            results = results.concat(_getAllFilesFromFolder(file))
+        } else results.push(file);
+
+    });
+
+    return results;
+
+};
+
+console.info(_getAllFilesFromFolder(".."));
